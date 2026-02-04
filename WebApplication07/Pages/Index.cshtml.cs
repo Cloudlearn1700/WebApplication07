b62bf84b850f5fd844fb4cfa3b79d0136b5fcb02
Kishore <kishorekumar17007@gmail.com>
2026-02-04 22:57:28 +0530

    Add project files.

---
+++ WebApplication07/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication07.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
